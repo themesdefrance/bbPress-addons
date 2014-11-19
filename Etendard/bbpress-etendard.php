576c01d0d9382d801e5e103b1faf64df6fbb8fca
alex_borto <themesdefrance@users.noreply.github.com>
2014-11-19 16:37:18 +0100

    Début integration licence

--- Etendard/bbpress-etendard.php
+++ Etendard/bbpress-etendard.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Étendard - Module bbPress
+Plugin Name: Etendard - Module bbPress
 Plugin URI: https://www.themesdefrance.fr/themes/etendard/
 Version: 1.0.0
 Description: Ce module permet d'adapter le thème WordPress Étendard pour le plugin bbPress.
@@ -35,6 +35,10 @@ define( 'ETENDARD_BBPRESS_DIR_PATH', plugin_dir_path( __FILE__ ) );
 define( 'ETENDARD_BBPRESS_DIR_URL',  plugin_dir_url( __FILE__ ) );
 define( 'ETENDARD_BBPRESS_VERSION',  '1.0.0' );
 
+define( 'EDD_SL_TDF_URL', 'https://www.themesdefrance.fr' );
+define( 'EDD_SL_ETENDARD_BBPRESS', 'Etendard - Module bbPress' );
+define( 'EDD_SL_ETENDARD_BBPRESS_LICENSE_KEY', 'etendard_bbpress_license_edd');
+
 // Compatibility check.
 require_once( ETENDARD_BBPRESS_DIR_PATH . 'includes/compatibility.php' );
 
@@ -48,7 +52,30 @@ require_once( ETENDARD_BBPRESS_DIR_PATH . 'includes/compatibility.php' );
 function etendard_bbp_load_textdomain() {
 	load_plugin_textdomain( 'etendard-bbpress', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' ); 
 }
-//add_action( 'plugins_loaded', 'etendard_bbp_load_textdomain' );
+add_action( 'plugins_loaded', 'etendard_bbp_load_textdomain' );
+
+// Load EDD Updater Class
+if( !class_exists( 'EDD_SL_Plugin_Updater' ) ) {
+	// load our custom updater
+	include( ETENDARD_BBPRESS_DIR_PATH . 'includes/EDD_SL_Plugin_Updater.php' );
+}
+
+function edd_sl_sample_plugin_updater() {
+
+	// retrieve our license key from the DB
+	$license_key = trim( get_option( EDD_SL_ETENDARD_BBPRESS_LICENSE_KEY ) );
+
+	// setup the updater
+	$edd_updater = new EDD_SL_Plugin_Updater( EDD_SL_TDF_URL, __FILE__, array( 
+			'version' 	=> ETENDARD_BBPRESS_VERSION, 
+			'license' 	=> $license_key,
+			'item_name' => EDD_SL_ETENDARD_BBPRESS,
+			'author' 	=> __('Themes de France','etendard-bbpress') 
+		)
+	);
+
+}
+add_action( 'admin_init', 'edd_sl_sample_plugin_updater', 0 );
 
 // Add new template stack. Yep, this is cool.
 require_once( ETENDARD_BBPRESS_DIR_PATH . 'includes/templates.php' );
